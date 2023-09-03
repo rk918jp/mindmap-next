4b8b2a4a8d3ee7b92e1e1a275c6e39d1d595d29b
rk918jp <113189714+rk918jp@users.noreply.github.com>
2023-09-03 14:00:59 +0900

    react-flow導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,53 @@
+import { AppBar, Box, ButtonGroup, Toolbar } from "@mui/material";
+import {
+  Background,
+  BackgroundVariant,
+  Controls,
+  MiniMap,
+  Panel,
+  ReactFlow,
+  useEdgesState,
+  useNodesState,
+} from "reactflow";
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 100, y: 100 }, data: { label: "1" } },
+  { id: "2", position: { x: 100, y: 200 }, data: { label: "2" } },
+];
+
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
 export default function Home() {
-  return <></>;
+  const [nodes, _setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, _setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  return (
+    <Box sx={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+      >
+        <Panel
+          position={"top-left"}
+          style={{
+            margin: 0,
+          }}
+        >
+          <Box sx={{ flexGrow: 1 }}>
+            <AppBar>
+              <Toolbar variant={"dense"}>
+                <ButtonGroup></ButtonGroup>
+              </Toolbar>
+            </AppBar>
+          </Box>
+        </Panel>
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+      </ReactFlow>
+    </Box>
+  );
 }
